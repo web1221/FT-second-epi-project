3b0a0fe21b801c09f3e05f35e635a6eea6e68cc5
Angela Weber <angelaweber95@hotmail.com>
2019-10-25 11:49:58 -0700

    start to explore a function for gathering value and showing result

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+languages = function(para1, para2, para3, para4) {
+
+}
+
 $(document).ready(function() {
   $('form#program-suggestor').submit(function(event) {
     event.preventDefault();
@@ -6,8 +10,8 @@ $(document).ready(function() {
     var question3 = parseInt($('input:radio[name=streaming]:checked').val());
     var question4 = parseInt($('input:radio[name=road-trip]:checked').val());
     var question5 = parseInt($('input:radio[name=reality]:checked').val());
-    console.log(question1, question2, question3, question4, question5);
-
+    console.log(question1 + question2 + question3 + question4 + question5);
+      // if (question1 =)
 
   });
 });
