4d947b5134f60f2d78b9b5f9dc03a06b93b99a94
Angela Weber <angelaweber95@hotmail.com>
2019-10-25 11:34:01 -0700

    finish making changes to input names and labels. add var question(1-5) in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
   $('form#program-suggestor').submit(function(event) {
-    var answerOptions = parseInt($("input:radio[name=operator]:checked").val());
+    event.preventDefault();
+    var question1 = parseInt($('input:radio[name=safest]:checked').val());
+    var question2 = parseInt($('input:radio[name=eggs]:checked').val());
+    var question3 = parseInt($('input:radio[name=streaming]:checked').val());
+    var question4 = parseInt($('input:radio[name=road-trip]:checked').val());
+    var question5 = parseInt($('input:radio[name=reality]:checked').val());
+    console.log(question1, question2, question3, question4, question5);
 
 
-  event.preventDeafult();
   });
 });
