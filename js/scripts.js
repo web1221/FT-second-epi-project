ddf0b882c0c0194937f8c22e934a2a7e14533606
Angela Weber <angelaweber95@hotmail.com>
2019-10-25 12:09:30 -0700

    add some logic to get outputs to show.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
-languages = function(para1, para2, para3, para4) {
-
-}
+// languages = function(para1, para2, para3, para4, para5) {
+//   var result = para1 + para2 + para3 + para4 + para5;
+//   if (result <= 7) {
+//     return true
+//   } else if result <= 12) {
+//
+//   }
+// }
 
 $(document).ready(function() {
   $('form#program-suggestor').submit(function(event) {
@@ -10,8 +15,25 @@ $(document).ready(function() {
     var question3 = parseInt($('input:radio[name=streaming]:checked').val());
     var question4 = parseInt($('input:radio[name=road-trip]:checked').val());
     var question5 = parseInt($('input:radio[name=reality]:checked').val());
-    console.log(question1 + question2 + question3 + question4 + question5);
-      // if (question1 =)
+    var result = question1 + question2 + question3 + question4 + question5;
+    console.log(question1 + question2 + question3 + question4 + question5)
+
+      if (result <= 7) {
+        $('.ruby').show();
+        $('.python, .javaScript, .c-sharp').hide();
+      } else if ((result <= 12) && (result > 7)) {
+        $('.python').show();
+        $('.ruby, .javaScript, .c-sharp').hide();
+      } else if ((result > 12) && (result <= 15)) {
+        $('.javaScript').show();
+        $('.ruby, .python, .c-sharp').hide();
+      } else {
+        $('.c-sharp').show();
+        $('.ruby, .python, .javaScript');
+      }
+
+      $('#output').show();
+
 
   });
 });
